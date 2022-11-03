df513d3bc223e7589663a398b72f8b1f1e8ee852
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2022-11-03 00:24:44 -0400

    added a way to see if todolist is empty in middleware

--- middleware/todo-middleware.js
+++ middleware/todo-middleware.js
@@ -3,6 +3,7 @@ const todo = require("../api/todo/toto-list/todo_list_model");
 module.exports = {
   validateTodo,
   validateTodoID,
+  validateTodoInList,
 };
 
 function validateTodoID(req, res, next) {
@@ -25,6 +26,21 @@ function validateTodoID(req, res, next) {
 function validateTodo(req, res, next) {
   if (!req.body.todo_list) {
     res.status(400).json({ message: "Todo not found!" });
+  } else {
+    next();
+  }
+}
+
+async function validateTodoInList(req, res, next) {
+  const { id } = req.params;
+
+  const todolist = await todo.findTodoListByTitleId(id);
+
+  if (todolist.length === 0) {
+    res
+      .status(404)
+      .json("There's nothing here. Lets put down something to do!!");
+    console.log(todolist);
   } else {
     next();
   }
